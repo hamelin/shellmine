1a469bb92fb6198d7664cac4f85463078b5f1516
Benoit Hamelin <benoit.hamelin@gmail.com>
2018-03-17 14:48:10 -0400

    Fix DB setup
    The relations needed by the shellmine user are now set up by that user,
    avoiding permission shenanigans.

--- setup.sh
+++ setup.sh
@@ -4,10 +4,11 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     create user shellmine password 'shellmine';
     create database shellmining;
     grant all privileges on database shellmining to shellmine;
-    \\connect shellmining;
+EOSQL
+psql -v ON_ERROR_STOP=1 --username shellmine --db shellmining <<-EOSQL
     create table cmdline(
         id  bigserial primary key,
         ts  timestamp,
-        cmd text[]
+        cmd text
     );
 EOSQL
